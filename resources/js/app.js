897a20b82e7b3566a04d1acffcdb4337c5da018a
rektph <kendrickjaviercosca@gmail.com>
2020-03-31 03:04:10 +0800

    scaffold for frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import App from '../vue/App';
+
+var app = new Vue({
+    el: "#app",
+    components: {
+        App
+    },
+    template: "<App/>"
+});
